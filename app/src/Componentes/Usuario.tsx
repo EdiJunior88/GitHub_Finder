b6110f741bbf12a8dd37fe2527f426c1ee8d5580
EdiJunior88 <er.moura88@yahoo.com.br>
2023-04-06 18:40:33 -0300

    Estilizando o card do usuário github

--- app/src/Componentes/Usuario.tsx
+++ app/src/Componentes/Usuario.tsx
@@ -10,15 +10,31 @@ const Usuario = ({
   following,
 }: InterfaceUsuario) => {
   return (
-    <div>
-      <p>{login}</p>
-      <img className="h-36 mx-auto border border-2 border-blue-950 rounded-lg" src={avatar_url} alt={login} />
-      <p>{location}</p>
-      <p>{bio}</p>
-      <p>Seguidores</p>
-      <p>{followers}</p>
-      <p>Seguindo</p>
-      <p>{following}</p>
+    <div className='container flex flex-col'>
+      <div>
+        <p>{login}</p>
+        <img
+          className='h-36 my-5 mx-auto border-2 border-blue-950 rounded-lg'
+          src={avatar_url}
+          alt={login}
+        />
+        <p>{location}</p>
+        <p className='my-5'>{bio}</p>
+      </div>
+
+      <div className='flex flex-row items-center justify-center gap-4'>
+        <div>
+          <p>Seguidores</p>
+          <p>{followers}</p>
+        </div>
+
+        <hr className='border border-blue-950 h-11' />
+
+        <div>
+          <p>Seguindo</p>
+          <p>{following}</p>
+        </div>
+      </div>
     </div>
   );
 };
