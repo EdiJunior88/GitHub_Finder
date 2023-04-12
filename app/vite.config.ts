b0064f9f537b98c85ebc0dabafa8cab6c188eff9
EdiJunior88 <er.moura88@yahoo.com.br>
2023-04-12 13:49:42 -0300

    Adicionando o pacote javascript-obfuscator - embaralhar o código JavaScript da aplicação - evita que pessoas copiem seu código fonte diretamente do console do navegador

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import javascriptObfuscator from "rollup-plugin-javascript-obfuscator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    javascriptObfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.75,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 0.4,
+      debugProtection: true,
+      debugProtectionInterval: true,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: "hexadecimal",
+      log: false,
+      renameGlobals: true,
+      rotateStringArray: true,
+      selfDefending: true,
+      shuffleStringArray: true,
+      splitStrings: true,
+      splitStringsChunkLength: 10,
+      stringArray: true,
+      stringArrayEncoding: ["base64"],
+      stringArrayThreshold: 0.75,
+      unicodeEscapeSequence: false,
+    }),
+  ],
+});
